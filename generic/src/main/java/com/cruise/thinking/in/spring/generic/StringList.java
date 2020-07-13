5171fd9841a29d7d5710139599cd2edb1c5ca756
Cruise <517552705@qq.com>
2020-07-13 18:54:03 +0800

    GenericTypeResolver 示例

---
+++ generic/src/main/java/com/cruise/thinking/in/spring/generic/StringList.java
@@ -0,0 +1,13 @@
+package com.cruise.thinking.in.spring.generic;
+
+import java.util.ArrayList;
+
+/**
+ * TODO
+ *
+ * @author Cruise
+ * @version 1.0
+ * @since 2020/7/13
+ */
+class StringList extends ArrayList<String> { // 泛型参数具体化（字节码有记录）
+}
